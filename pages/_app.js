92f9820b4644a0df7bdc7fa2d2d8fc97a9bd2336
Adhil Roshan <58517896+adhilroshan@users.noreply.github.com>
2024-04-09 07:45:59 +0530

    Update favicon link in head.js and _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -7,12 +7,11 @@ const MyApp = ({ Component, pageProps }) => (
     <Head>
       <title>Conclave 2024</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="preconnect" href="https://stijndv.com" />
       <link
         rel="stylesheet"
         href="https://stijndv.com/fonts/Eudoxus-Sans.css"
       />
-      <link rel="icon" type="image/x-icon" href="/conclave.png" />
+      <link rel="icon" type="image/x-icon" href="/conc.png" />
     </Head>
     <Component {...pageProps} />
   </>
